b92f672cf335bdf113e0e0ac559f2ee92fafd95e
1122pcd1122 <1132420795@qq.com>
2025-12-05 11:31:02 +0800

    更新README.md

--- src/main/java/com/example/gujihuohua/mapper/RelationFactMapper.java
+++
@@ -1,8 +0,0 @@
-package com.example.gujihuohua.mapper;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.example.gujihuohua.entity.RelationFact;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface RelationFactMapper extends BaseMapper<RelationFact> {
-}
--- src/main/java/com/example/gujihuohua/mapper/XinhuaWordMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.gujihuohua.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.example.gujihuohua.entity.XinhuaWord;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface XinhuaWordMapper extends BaseMapper<XinhuaWord> {
-}
